44de9d70c17cc5f796cf19bf2146e7c352a0eb11
Matte <51192395+MatteZ02@users.noreply.github.com>
2024-04-18 15:27:43 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,28 +2,11 @@ import express from "express";
 import cors from "cors";
 import api from "./api";
 import { errorHandler, notFoundHandler } from "./core/middleware/errorHandler";
-import https from "https";
-import debux from "debux";
-import httpsOptions from "./config/httpsOptions";
 import rateLimit from "./core/middleware/ratelimit";
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-    https
-        .createServer(httpsOptions, app)
-        .listen(443, () => debux().info("Server listening on port 443"));
-
-    app.use((request, response, next) => {
-        if (!request.secure) {
-            return response.redirect("https://" + request.headers.host + request.url);
-        }
-
-        next();
-    });
-
-    app.use("/", express.static("../frontend/dist"));
-}
+if (process.env.NODE_ENV === "production") app.use("/", express.static("../frontend/dist"));
 
 app.use(cors());
 app.use(express.json());
