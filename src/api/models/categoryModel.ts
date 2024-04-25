d6f4d0f89f4ee921acf30b4f6e0023858a64f5b1
Matte <51192395+MatteZ02@users.noreply.github.com>
2024-04-25 11:36:57 +0300

    set listing categories to 0 when a category gets deleted

--- src/api/models/categoryModel.ts
+++ src/api/models/categoryModel.ts
@@ -23,6 +23,7 @@ const addCategory = async (categoryData: Category): Promise<PostCategoryResponse
 };
 
 const deleteCategory = async (id: number): Promise<number | null> => {
+    Database.query("UPDATE listings SET category = 0 WHERE category = ?", [id]);
     const deleted = await Database.delete("categories", id);
     return deleted ? id : null;
 };
