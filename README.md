c77025e83e48807c73c57e66c11a8ab1c5b98d32
Matte <51192395+MatteZ02@users.noreply.github.com>
2024-08-26 11:40:04 +0300

    add docker compose

---
+++ README.md
@@ -0,0 +1,7 @@
+# Added a way to run with docker compose
+Added compose.yml file and changed the address in index.ts file.
+
+## used commands
+```sh
+docker-compose up --build
+```
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import config from "./config";
 import app from "./app";
 import debux from "debux";
 
-app.listen(config.port, () => {
+app.listen(+config.port, "0.0.0.0", () => {
     debux().info(`Listening on port: ${config.port}`, {
         class: "app",
         function: "listen",
